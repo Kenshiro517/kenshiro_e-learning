57c5e1d21eedc907e96e3962894835953ab51642
Shiro <61719297+Kenshiro517@users.noreply.github.com>
2020-03-10 09:38:12 +0800

    finish create-signup (#3)
    * finish create-signup
    
    * finish create-signup
    
    Co-authored-by: 大久保健志郎 <okubokenshirou@ookubotakeshirou-no-MacBook-Air.local>

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,20 @@
+class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+  
+  def create
+    @user = User.new(user_params)    # Not the final implementation!
+    if @user.save
+      flash[:success] ="Welcome to the E-learning!"
+      redirect_to root_url
+    else
+      render 'new'
+    end
+  end  
+  
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+
+end
