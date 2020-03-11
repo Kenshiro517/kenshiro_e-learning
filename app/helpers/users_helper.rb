a14b8aece2164dad1b3ba76f05ad3c8f702d4b70
大久保健志郎 <okubokenshirou@ookubotakeshirou-no-MacBook-Air.local>
2020-03-11 11:52:27 +0800

    finish user-showpage

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,11 @@
 module UsersHelper
+  def user_avatar(user,classes)
+    @avatar= user.avatar
+    if @avatar.present?
+      @avatar_user = image_tag( @avatar.url, class: classes)
+    else
+      @avatar_user = image_tag( 'default.jpg', class: classes)
+    end
+  end
+
 end
