bc3df6c1efe385916ebf36a2645d94bc54bb4154
大久保健志郎 <okubokenshirou@ookubotakeshirou-no-MacBook-Air.local>
2020-03-11 10:35:42 +0800

    finish edit-profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,9 @@
 class UsersController < ApplicationController
+
+
+  before_action :correct_user,only: [:edit, :update]
+
+
   def new
     @user = User.new
   end
@@ -34,5 +39,10 @@ class UsersController < ApplicationController
   private
     def user_params
       params.require(:user).permit(:name, :email, :password, :password_confirmation, :avatar)
+    end
+
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
     end
 end
