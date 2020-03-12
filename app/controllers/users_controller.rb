c68abdb3a70fa0e341333af8b7238e40efada61f
Shiro <61719297+Kenshiro517@users.noreply.github.com>
2020-03-12 09:34:51 +0800

    finish user-sho-page (#7)
    * finish user-sho-page
    
    * fix indention
    
    Co-authored-by: 大久保健志郎 <okubokenshirou@ookubotakeshirou-no-MacBook-Air.local>

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -36,6 +36,10 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def index
+    @users = User.paginate(page: params[:page], per_page: 10)
+  end
+
   private
     def user_params
       params.require(:user).permit(:name, :email, :password, :password_confirmation, :avatar)
