6a8c07e770668051a51270f28a1d45dc3d27fdb8
大久保健志郎 <okubokenshirou@ookubotakeshirou-no-MacBook-Air.local>
2020-03-17 14:10:41 +0800

    edit-delete-category

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -21,6 +21,27 @@ class Admin::CategoriesController < ApplicationController
     @categories = Category.paginate(page: params[:page], per_page: 10)
   end
  
+  def edit
+    @category = Category.find_by(id: params[:id])
+  end
+
+  def update
+    @category = Category.find(params[:id])
+    if @category.update_attributes(category_params)
+      flash[:success] ="Successfully updated!"
+      redirect_to admin_categories_url
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @category = Category.find(params[:id])
+    @category.destroy
+    flash[:danger] ="You deleted a category!"
+    redirect_to admin_categories_url
+  end
+ 
   private
     def category_params
       params.require(:category).permit(:title, :description)
