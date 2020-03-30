a013dffa77623058a77e154450fd960eb8731e04
大久保健志郎 <okubokenshirou@ookubotakeshirou-no-MacBook-Air.local>
2020-03-30 11:52:23 +0900

    activity-feed

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,12 +1,13 @@
 class StaticPagesController < ApplicationController
   def home
     if logged_in?
-      @activities = Activity.where("user_id IN (?) OR user_id = ?",current_user.followings.ids,current_user.id)
-      
+      @activities = Activity.where("user_id IN (?) OR user_id = ?",current_user.followings.ids,current_user.id).paginate(page: params[:page], per_page: 10).order(updated_at: :desc)
       render 'users/users_feeds'
     end
   end
-
+  
   def about
   end
 end
+
+
